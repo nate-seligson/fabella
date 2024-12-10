3c93e08f3f4b8beb1d94c884bff76fbecead5e6c
nate-seligson <146794258+nate-seligson@users.noreply.github.com>
2024-12-09 20:00:00 -0500

    day 1

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 import { Story } from "./story.js";
-import { turnPage } from "./pageturner.js";
+import { turnPage, goBack } from "./pageturner.js";
+import { updateCountdown } from "./countdown.js";
 const x = new Story();
 document.getElementById("next").addEventListener("click", function(){turnPage(x)})
-turnPage(x);
+document.getElementById("back").addEventListener("click", function(){goBack(x)})
+
+x.writeSlice("l", 0);
+x.writeSlice("r",1);
+
+(async() => {await x.checkLoaded();
+    document.getElementById("pagecount").innerHTML = `Page 0   out of ${x.maxPages + 1}`
+})();
+setInterval(function(){document.getElementById("timer").innerHTML = updateCountdown()}, 1000);
+updateCountdown();
--- pageturner.js
+++ pageturner.js
@@ -1,16 +1,26 @@
 let index = 0;
 export function turnPage(story){
+    index+=2;
     story.writeSlice("l", index);
     story.writeSlice("r",index + 1);
 
     //update page count
-    (async() => {await story.checkLoaded();
     if(index < story.maxPages){
-        document.getElementById("pagecount").innerHTML = `${index} / ${story.maxPages + 1}`
+        document.getElementById("pagecount").innerHTML = `Page ${index}   out of ${story.maxPages + 1}`
     }
     else{
-        document.getElementById("pagecount").innerHTML = `${story.maxPages + 1} / ${story.maxPages + 1}`
+        document.getElementById("pagecount").innerHTML = `Page ${story.maxPages + 1}   out of ${story.maxPages + 1}`
+        index = story.maxPages + 1;
     }
-    index+=2;
-})();
+}
+export function goBack(story){
+    if(index <= 0){
+        return;
+    }
+    story.writeSlice("l", index-2);
+    story.writeSlice("r",index - 1);
+
+    //update page count
+    index-=2
+    document.getElementById("pagecount").innerHTML = `Page ${index}   out of ${story.maxPages + 1}`
 }
